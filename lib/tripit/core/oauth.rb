289784b11f75679c2ae7fa439aebc69f56a2de26
Carlos Nunez <carlosonunez@noreply.github.com>
2019-11-24 14:38:00 -0600

    Generate OAuth authorization headers

--- lib/tripit/core/oauth.rb
+++ lib/tripit/core/oauth.rb
@@ -6,8 +6,29 @@ require 'tripit/core/api'
 module TripIt
   module Core
     module OAuth
+      def self.generate_request_auth_headers(consumer_key:,
+                                             consumer_secret:,
+                                             nonce:,
+                                             timestamp:)
+        signature = self.generate_signature_for_request_token(
+          consumer_key: consumer_key,
+          consumer_secret: consumer_secret,
+          nonce: nonce,
+          timestamp: timestamp)
+        headers = {
+          realm: 'https://api.tripit.com/oauth/request_token',
+          oauth_consumer_key: consumer_key,
+          oauth_nonce: nonce,
+          oauth_timestamp: timestamp,
+          oauth_signature_method: 'HMAC-SHA1',
+          oauth_version: '1.0',
+          oauth_signature: signature,
+        }
+        return "OAuth " + headers.map{|k,v| "#{k}=#{Base64.encode64(v)}"}.join(',')
+      end
+
       # Since this uses OAuth v1, we need to generate a signature by hand.
-      def self.generate_signature_for_request_token(consumer_key:, nonce:, timestamp:)
+      def self.generate_signature_for_request_token(consumer_key:, consumer_secret:, nonce:, timestamp:)
         method = 'GET'
         uri = "https://api.tripit.com/oauth/request_token"
         params = {
@@ -19,11 +40,12 @@ module TripIt
         }
         params_serialized = params.map{|key,value| "#{key}=#{value}"}.join('&')
         sig_base_string = "#{method}&#{CGI.escape uri}&#{CGI.escape params_serialized}"
-        encrypt_key = '&' # The TripIt API docs don't seem to require secrets here.
+        encrypt_key = "#{consumer_secret}&" # This isn't in the TripIt documentation!
         raw_signature = OpenSSL::HMAC.digest('SHA1', encrypt_key, sig_base_string)
         encoded_signature = Base64.encode64(raw_signature).strip
         return encoded_signature
       end
+
       # Retrieve an OAuth token from a given code and client ID/secret.
       def self.access(client_id:, client_secret:, code:, redirect_uri:)
         params = {
--- spec/unit/tripit_auth_spec.rb
+++ spec/unit/tripit_auth_spec.rb
@@ -1,78 +1,53 @@
 require 'spec_helper'
-require 'ostruct'
+require 'base64'
 
 describe "TripIt OAuth" do
-  context "OAuth v1 Signatures" do
+  context "OAuth v1 request tokens" do
     it "Should yield a correct signature", :unit do
-      expected_signature = "aTe5fXaR5Nfb2ukwebPEuNPLJNs="
-      oauth_consumer_key = 'fake-key'
+      expected_signature = "wr9/w0ruXKUD6KZvd2QmKX1KYaA="
+      oauth_consumer_key = ENV['TRIPIT_APP_CLIENT_ID'] || raise("No client ID found.")
+      oauth_consumer_secret = ENV['TRIPIT_APP_CLIENT_SECRET'] || raise("No client secret found.")
       oauth_nonce = 'fake-nonce'
       oauth_timestamp = '1574621346'
-      expect(TripIt::Core::OAuth.generate_signature_for_request_token(consumer_key: oauth_consumer_key,
-                                                                      nonce: oauth_nonce,
-                                                                      timestamp: oauth_timestamp))
+      expect(
+        TripIt::Core::OAuth.generate_signature_for_request_token(consumer_key: oauth_consumer_key,
+                                                                 consumer_secret: oauth_consumer_secret,
+                                                                 nonce: oauth_nonce,
+                                                                 timestamp: oauth_timestamp))
         .to eq expected_signature
     end
-  end
-  context 'Handling state associations' do
-    it "Should save access keys with state IDs", :wip do
-      Helpers::Aws::DynamoDBLocal.drop_tables!
-      fake_event = JSON.parse({
-        requestContext: {
-          identity: {
-            apiKey: 'fake-key'
-          }
-        }
-      }.to_json)
-      TripIt::Auth.associate_access_key_to_state_id!(event: fake_event,
-                                                       state_id: 'fake-state-id')
-      expect(TripIt::Auth.get_access_key_from_state(state_id: 'fake-state-id'))
-        .to eq 'fake-key'
+
+    it "Should yield correct authorization headers", :unit do
+      expected_signature = "wr9/w0ruXKUD6KZvd2QmKX1KYaA="
+      oauth_consumer_key = ENV['TRIPIT_APP_CLIENT_ID'] || raise("No client ID found.")
+      oauth_consumer_secret = ENV['TRIPIT_APP_CLIENT_SECRET'] || raise("No client secret found.")
+      oauth_nonce = 'fake-nonce'
+      oauth_timestamp = '1574621346'
+      expected_auth_headers_hash = {
+        realm: 'https://api.tripit.com/oauth/request_token',
+        oauth_consumer_key: oauth_consumer_key,
+        oauth_nonce: oauth_nonce,
+        oauth_timestamp: oauth_timestamp,
+        oauth_signature_method: 'HMAC-SHA1',
+        oauth_version: '1.0',
+        oauth_signature: expected_signature,
+      }
+      expected_auth_headers = "OAuth " + expected_auth_headers_hash.map {|key, value|
+        "#{key}=#{Base64.encode64(value)}"
+      }.join(',')
+      expect(TripIt::Core::OAuth.generate_request_auth_headers(consumer_key: oauth_consumer_key,
+                                                               consumer_secret: oauth_consumer_secret,
+                                                               nonce: oauth_nonce,
+                                                               timestamp: oauth_timestamp))
+        .to eq expected_auth_headers
     end
   end
 
-  context 'Handling tokens' do
-    it "Should give me an error message Retrieving tokens while not authenticated", :wip do
-      Helpers::Aws::DynamoDBLocal.drop_tables!
-      fake_event = JSON.parse({
-        requestContext: {
-          identity: {
-            apiKey: 'fake-key'
-          }
-        }
-      }.to_json)
-      expected_response = {
-        statusCode: 404,
-        body: { status: 'error', message: 'No token exists for this access key.' }.to_json
-      }
-      expect(TripIt::Auth::get_tripit_token(event: fake_event)).to eq expected_response
-    end
-
-    it "Should persist tokens with their associated API keys", :wip do
-      fake_event = JSON.parse({
-        requestContext: {
-          identity: {
-            apiKey: 'fake-key'
-          }
-        }
-      }.to_json)
-      expected_get_response = {
-        statusCode: 200,
-        body: { status: 'ok', token: 'fake' }.to_json
-      }
-      expect(TripIt::Auth::put_tripit_token(access_key: 'fake-key',
-                                             tripit_token: 'fake')).to be true
-      expect(TripIt::Auth::get_tripit_token(event: fake_event)).to eq expected_get_response
-    end
-  end
   context "We aren't authenticated yet" do
-    it "Should give the user an auth init prompt without providing a workspace", :wip do
+    it "Should give the user an auth init prompt", :unit do
       Helpers::Aws::DynamoDBLocal.drop_tables!
-      expect(SecureRandom).to receive(:hex).and_return('fake-state-id')
+      ENV['TRIPIT_CONSUMER_PUBLIC_KEY'] = 'fake-key'
       fake_event = JSON.parse({
-        queryStringParameters: {
-          workspace: 'fake'
-        },
         requestContext: {
           path: '/develop/auth',
           identity: {
@@ -85,10 +60,8 @@ describe "TripIt OAuth" do
       }.to_json)
       expected_message = "You will need to authenticate into TripIt first; \
 click on or copy/paste this URL to get started: \
-https://fake.tripit.com/oauth/authorize?client_id=fake&\
-scope=users.profile:read,users.profile:write&\
-redirect_uri=https://example.fake/develop/callback&\
-state=fake-state-id"
+https://www.tripit.com/oauth/authorize?oauth_token=fake-token&\
+oauth_callback=https://example.fake/develop/callback"
       expected_response = {
         statusCode: 200,
         body: { status: 'ok', message: expected_message }.to_json
@@ -96,8 +69,10 @@ state=fake-state-id"
       expect(TripIt::Auth::begin_authentication_flow(fake_event,
                                                        client_id: 'fake'))
         .to eq expected_response
-      expect(TripIt::Auth.get_access_key_from_state(state_id: 'fake-state-id'))
+      expect(TripIt::Auth.get_access_key_from_state(oauth_token: 'fake-token'))
         .to eq 'fake-key'
+      expect(TripIt::Auth.get_token_secret_from_state(oauth_token: 'fake-token'))
+        .to eq 'fake-secret'
     end
 
     it "Should give the user an auth init prompt", :wip do
@@ -218,6 +193,57 @@ state=fake-state-id"
         }.to_json))
       allow(TripIt::Auth).to receive(:get_access_key_from_state).and_return('fake-key')
       expect(TripIt::Auth::handle_callback(fake_event)).to eq expected_response
+    end
+  end
+  context 'Handling state associations' do
+    it "Should save access keys with state IDs", :wip do
+      Helpers::Aws::DynamoDBLocal.drop_tables!
+      fake_event = JSON.parse({
+        requestContext: {
+          identity: {
+            apiKey: 'fake-key'
+          }
+        }
+      }.to_json)
+      TripIt::Auth.associate_access_key_to_state_id!(event: fake_event,
+                                                       state_id: 'fake-state-id')
+      expect(TripIt::Auth.get_access_key_from_state(state_id: 'fake-state-id'))
+        .to eq 'fake-key'
+    end
+  end
+
+  context 'Handling tokens' do
+    it "Should give me an error message Retrieving tokens while not authenticated", :wip do
+      Helpers::Aws::DynamoDBLocal.drop_tables!
+      fake_event = JSON.parse({
+        requestContext: {
+          identity: {
+            apiKey: 'fake-key'
+          }
+        }
+      }.to_json)
+      expected_response = {
+        statusCode: 404,
+        body: { status: 'error', message: 'No token exists for this access key.' }.to_json
+      }
+      expect(TripIt::Auth::get_tripit_token(event: fake_event)).to eq expected_response
+    end
+
+    it "Should persist tokens with their associated API keys", :wip do
+      fake_event = JSON.parse({
+        requestContext: {
+          identity: {
+            apiKey: 'fake-key'
+          }
+        }
+      }.to_json)
+      expected_get_response = {
+        statusCode: 200,
+        body: { status: 'ok', token: 'fake' }.to_json
+      }
+      expect(TripIt::Auth::put_tripit_token(access_key: 'fake-key',
+                                             tripit_token: 'fake')).to be true
+      expect(TripIt::Auth::get_tripit_token(event: fake_event)).to eq expected_get_response
     end
   end
 end
