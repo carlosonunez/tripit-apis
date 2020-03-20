7e6a814430349e6c9ac911c7f7483a164786b651
Carlos Nunez <carlosonunez@noreply.github.com>
2020-03-20 17:58:04 -0500

    Add our first unit test for helf

---
+++ tests/unit/ping_test.py
@@ -0,0 +1,19 @@
+"""
+Healthiness tests.
+"""
+import json
+import pytest
+from tripit.health import ping
+
+
+@pytest.mark.unit
+def test_ping():
+    """
+    Basic healthiness function to indicate that all is well
+    with our API.
+    """
+    response = {
+        "body": json.loads({"message": "sup dawg"}),
+        "statusCode": 200
+    }
+    assert ping() == response
