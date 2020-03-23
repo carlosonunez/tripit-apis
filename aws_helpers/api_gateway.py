3a20650821d6fe1f7d022736f5f8fd1c3bd6896a
Carlos Nunez <carlosonunez@noreply.github.com>
2020-03-23 10:16:21 -0500

    Simplify return_ok

--- aws_helpers/api_gateway.py
+++ aws_helpers/api_gateway.py
@@ -9,14 +9,12 @@ def return_ok(message=None, additional_json=None):
     """
     Returns HTTP 200 with a custom message and additional JSON, if desired.
     """
-    if message is None and additional_json is None:
-        return return_200(json_payload={"status": "ok"})
-    if message is None:
-        return return_200(json_payload={**{"status": "ok"}, **additional_json})
-    if additional_json is None:
-        return return_200(json_payload={"status": "ok", "message": message})
-    return return_200(json_payload={**{"status": "ok", "message": message},
-                                    **additional_json})
+    payload = {"status": "ok"}
+    if additional_json is not None:
+        payload = {**payload, **additional_json}
+    if message is not None:
+        payload["message"] = message
+    return return_200(payload)
 
 
 def return_error(message=None, code=400):
