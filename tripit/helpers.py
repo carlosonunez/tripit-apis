31daa55e97e9f1239d11a77646b7dd3389f85c8f
Carlos Nunez <carlosonunez@noreply.github.com>
2020-03-29 22:12:47 -0500

    Add some convenient helpers

---
+++ tripit/helpers.py
@@ -0,0 +1,8 @@
+"""
+Helper functions used throughout this project.
+"""
+
+
+def sort_dict(dict_to_sort):
+    """ Sorts a dict alphabetically in increasing order. """
+    return dict(sorted(dict_to_sort.items()))
