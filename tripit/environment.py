8591c05c00569a60b8160128582a04e724ffa118
Carlos Nunez <carlosonunez@noreply.github.com>
2020-03-29 21:15:05 -0500

    Add environment checking functions

---
+++ tripit/environment.py
@@ -0,0 +1,27 @@
+"""
+Useful functions for verifying environments.
+"""
+import os
+
+
+class EnvironmentCheck:  # pylint: disable=too-few-public-methods
+    """
+    Class for ensuring that an environment has been properly initialized.
+    Disabling this because this class doesn't yet have multiple methods, but
+    I am anticipating that it will, and I don't want to rewrite environment
+    var checking code in multiple modules.
+    """
+
+    def __init__(self, vars_to_check):
+        self.ready = False
+        self.missing_vars = []
+        self.__check_environment(vars_to_check)
+
+    def check_environment(self, vars_to_check):
+        """
+        Verifies that all env vars are defined
+        """
+        self.missing_vars = [var for var in vars_to_check if os.environ.get(var) is None]
+        self.ready = self.missing_vars is None
+
+    __check_environment = check_environment
