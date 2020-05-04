d8eb7179121b8ae79d843a6dfabccf8bc7e7dfee
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-04 17:31:31 -0500

    Switch to pytest_plugins; more supported

--- conftest.py
+++ conftest.py
@@ -4,4 +4,7 @@ Fixtures and stuff.
 
 # This is a magic variable used by Pytest to load modularized fixtures.
 # pylint: disable=invalid-name
-pytest_plugins = ["tests.fixtures.fake_response"]
+pytest_plugins = [
+    "tests.fixtures.fake_response",
+    "tests.fixtures.token_state_table",
+]
