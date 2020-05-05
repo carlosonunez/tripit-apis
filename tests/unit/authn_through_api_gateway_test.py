bb80dad2f50b597d82f4257733ba5d2f98b6bf88
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-04 22:00:51 -0500

    Add reauthorize and complete step 1

--- tests/unit/authn_through_api_gateway_test.py
+++ tests/unit/authn_through_api_gateway_test.py
@@ -54,3 +54,33 @@ def test_generating_auth_url_with_tokens(monkeypatch):
     """
     monkeypatch.setattr("tripit.auth.auth_flow.access_key_has_token", lambda *args, **kwargs: True)
     assert get_authn_url(access_key="fake-key", api_gateway_endpoint="foo") is None
+
+
+@pytest.mark.unit
+def test_asking_for_reauthorization(monkeypatch, query_request_token_table):
+    """
+    If we already have tokens during step 1 of the authentication process, but we ask
+    to reauthorize, we should delete what's in the tokens table and begin reauthenticating.
+    """
+    request_tokens = lambda: {"token": "fake-request-token", "token_secret": "fake-secret"}
+    monkeypatch.setattr("tripit.auth.auth_flow.access_key_has_token", lambda *args, **kwargs: True)
+    monkeypatch.setattr("tripit.core.v1.oauth.fetch_token", request_tokens)
+    expected_callback_url = urllib.parse.urlunparse(("https", "foo", "/callback", "", "", ""))
+    expected_url = urllib.parse.urlunparse(
+        (
+            "https",
+            "www.tripit.com",
+            "/oauth/authorize",
+            "",
+            "".join(["oauth_token=fake-request-token&oauth_callback=", expected_callback_url]),
+            "",
+        )
+    )
+    url = get_authn_url(access_key="fake-key", api_gateway_endpoint="foo", reauthorize=True)
+    assert url == expected_url
+    request_token_mapping = query_request_token_table("fake-key")
+    assert request_token_mapping == {
+        "access_key": "fake-key",
+        "token": "fake-request-token",
+        "token_secret": "fake-secret",
+    }
--- tripit/auth/auth_flow.py
+++ tripit/auth/auth_flow.py
@@ -10,18 +10,21 @@ from tripit.auth.models import TripitRequestToken
 from tripit.logging import logger
 
 
-def get_authn_url(api_gateway_endpoint, access_key):
+def get_authn_url(api_gateway_endpoint, access_key, reauthorize=False):
     """
     Generates an authentication URL for an access key if no request or access
     tokens exist for it.
     """
-    if access_key_has_token(access_key):
+    if access_key_has_token(access_key) and not reauthorize:
         return None
+    if reauthorize:
+        delete_existing_request_tokens(access_key)
 
     token_data = request_request_token()
     if token_data is None:
         logger.error("Unable to get token; see previous logs for why.")
         return None
+
     callback_url = urllib.parse.urlunparse(("https", api_gateway_endpoint, "callback", "", "", ""))
     auth_url = urllib.parse.urlunparse(
         (
@@ -66,3 +69,18 @@ def associate_access_key_with_request_token(access_key, token, token_secret):
     )
     new_request_token_mapping.save()
     new_request_token_mapping.refresh()
+
+
+def delete_existing_request_tokens(access_key):
+    """
+    Delete request tokens associated with an access key, if any found.
+    """
+    try:
+        existing_request_token_mapping = TripitRequestToken.get(access_key)
+        existing_request_token_mapping.delete()
+        existing_request_token_mapping.save()
+        existing_request_token_mapping.refresh()
+        return True
+    except TripitRequestToken.DoesNotExist:
+        logger.warning("Potential bug --- tried to delete request tokens for an absent access key")
+        return None
