33ed56da8d485dc10b68ab90d974d6a51cad10d9
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-08 13:45:13 -0500

    Port over `/trips`

---
+++ tests/unit/api/trips_test.py
@@ -0,0 +1,21 @@
+"""
+Tests for getting all trips endpoint.
+"""
+import json
+import pytest
+from tripit.api.aws_api_gateway.trips import get_trips
+
+
+@pytest.mark.unit
+def test_trips_endpoint_unauthenticated():
+    """
+    Gets all of the trips associated with ones Tripit account.
+    """
+    fake_event = {
+        "requestContext": {"path": "/develop/trips", "identity": {"apiKey": "fake-key"},},
+    }
+    expected_response = {
+        "statusCode": 403,
+        "body": json.dumps({"status": "error", "message": "Access denied; run /auth first."}),
+    }
+    assert get_trips(fake_event) == expected_response
---
+++ tripit/api/aws_api_gateway/trips.py
@@ -0,0 +1,10 @@
+"""
+Functions for working with trips.
+"""
+
+
+def get_trips(event):
+    """
+    Gets all trips associated with a TripIt account.
+    """
+    return event
