598fd9a349d39fda4be354e6bb0e548dbf43fb50
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-09 13:30:32 -0500

    Add our test for pinging our endpoint.

--- tests/integration/ping_test.py
+++ tests/integration/ping_test.py
@@ -7,8 +7,9 @@ import requests
 
 
 @pytest.mark.integration
-def ping_test(create_api_gateway_url):
+def test_ping(create_api_gateway_url):
     """ See module doc. """
-    response = requests.get(create_api_gateway_url("/ping"))
+    url = create_api_gateway_url("ping")
+    response = requests.get(url)
     assert response.status_code == 200
     assert response.json == {"status": "ok"}
