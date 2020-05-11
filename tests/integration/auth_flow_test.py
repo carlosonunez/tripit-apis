cd90116baf83bf87b4bf848fa287c790d67c7798
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-11 17:25:02 -0500

    Add test for handling callbacks.

--- tests/integration/auth_flow_test.py
+++ tests/integration/auth_flow_test.py
@@ -2,12 +2,15 @@
 This tests covers the entire flow of authenticating into the TripIt API and
 authorizing this application to access their TripIt account details.
 """
-
+import json
+import os
 import re
 import time
 import pytest
 import requests
 import timeout_decorator
+from tripit.logging import logger
+from tests.fixtures.integration.tripit_web_driver import TripitWebDriver
 
 
 @timeout_decorator.timeout(5)
@@ -24,6 +27,29 @@ def get_and_wait_for_lambda_ready(url, access_key=None):
         if response.status_code != 403:
             return response
         time.sleep(1)
+
+
+def authorize_tripit(authz_url):
+    """
+    Authorizes TripIt through an automated web browser.
+    """
+    session = TripitWebDriver()
+    try:
+        session.visit(authz_url)
+        session.fill_in("email_address", os.environ.get("TRIPIT_SANDBOX_ACCOUNT_EMAIL"))
+        session.fill_in("password", os.environ.get("TRIPIT_SANDBOX_ACCOUNT_PASSWORD"))
+        session.click_button("Sign In")
+        junk_prefix = (
+            '<html><head></head><body><pre style="word-wrap: break-word; white-space: pre-wrap;">'
+        )
+        junk_postfix = "</pre></body></html>"
+        result = session.page().replace(junk_prefix, "").replace(junk_postfix, "")
+        return json.loads(result)
+    # We want a broad exception here.
+    # pylint: disable=broad-except
+    except Exception as failure:
+        logger.error("Failed to authorize: %s", failure)
+        return None
 
 
 @pytest.mark.integration
@@ -54,5 +80,26 @@ def test_getting_an_authorization_url(create_api_gateway_url, access_key):
     assert len(matches.groups()) == 1
 
 
-# @pytest.mark.integration
-# def test_getting_a_token_after_callback(create_api_gateway_url, access_key):
+@pytest.mark.integration
+def test_getting_a_token_after_callback(create_api_gateway_url, access_key):
+    """
+    Do we get an OAuth token back after authorizing Tripit?
+    """
+    expected_callback_url = create_api_gateway_url("callback")
+    tripit_auth_url_pattern = re.compile(
+        "".join(
+            [
+                ".*",
+                "(https://www.tripit.com/oauth/authorize\?oauth_token=[a-zA-Z0-9]+&",
+                "oauth_callback=",
+                expected_callback_url,
+                ")",
+            ]
+        )
+    )
+    response = get_and_wait_for_lambda_ready(create_api_gateway_url("auth"), access_key)
+    matches = tripit_auth_url_pattern.match(response.json()["message"])
+    authz_url = matches.groups()[0]
+    expected_callback_response = authorize_tripit(authz_url + "&is_sign_in=1")
+    assert "status" in expected_callback_response
+    assert expected_callback_response["status"] == "ok"
