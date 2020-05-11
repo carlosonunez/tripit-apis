54ada9e2cb2fde70563511915c5df3840411b07d
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-11 17:24:55 -0500

    This should actually handle getting the response

--- tests/integration/auth_flow_test.py
+++ tests/integration/auth_flow_test.py
@@ -11,12 +11,18 @@ import timeout_decorator
 
 
 @timeout_decorator.timeout(5)
-def wait_until_lambda_ready(response):
+def get_and_wait_for_lambda_ready(url, access_key=None):
     """
     We might get 403 Forbidden's while Lambda starts up.
     This waits for them to clear out.
     """
-    if response.status_code == 403:
+    while True:
+        headers = {}
+        if access_key:
+            headers = {"x-api-key": access_key}
+        response = requests.get(url, headers=headers)
+        if response.status_code != 403:
+            return response
         time.sleep(1)
 
 
@@ -25,9 +31,8 @@ def test_getting_an_authorization_url(create_api_gateway_url, access_key):
     """
     Do we get a URL that we can click on to authorize our application?
     """
-    auth_url = create_api_gateway_url("auth") + "?reauthorize=true"
     expected_callback_url = create_api_gateway_url("callback")
-    response = requests.get(auth_url, headers={"x-api-key": access_key})
+    response = get_and_wait_for_lambda_ready(create_api_gateway_url("auth"), access_key)
 
     # pylint doesn't know that this will be compiled into a regex
     # pylint:disable=anomalous-backslash-in-string
@@ -42,7 +47,6 @@ def test_getting_an_authorization_url(create_api_gateway_url, access_key):
             ]
         )
     )
-    wait_until_lambda_ready(response)
     assert response.status_code == 200
 
     matches = expected_tripit_auth_url_pattern.match(response.json()["message"])
