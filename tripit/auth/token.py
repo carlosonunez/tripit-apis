d0a7382bffe7f237171c3f12823a4b32de9970df
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-12 17:40:31 -0500

    This should return None if no tokens found

--- tripit/auth/token.py
+++ tripit/auth/token.py
@@ -2,10 +2,15 @@
 Handles retrieving tokens from access keys.
 """
 from tripit.auth.models import TripitAccessToken
+from tripit.logging import logger
 
 
 def get_token_data_for_access_key(access_key):
     """
     Fetches a token for an access key, if it has one.
     """
-    return TripitAccessToken.as_dict(access_key)
+    try:
+        return TripitAccessToken.as_dict(access_key)
+    except Exception as access_token_error:
+        logger.warning("Failed to get an access token: %s", access_token_error)
+        return None
