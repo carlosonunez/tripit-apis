a79a4be44cc6d751be3017e96adb21cee88bfac7
Carlos Nunez <carlosonunez@noreply.github.com>
2020-05-12 13:29:22 -0500

    Add fixture for setting and deleting current trips

--- conftest.py
+++ conftest.py
@@ -11,6 +11,7 @@ pytest_plugins = [
     "tests.fixtures.unit.access_token_state_table",
     "tests.fixtures.unit.show_as_human_readable_date",
     "tests.fixtures.integration.authorize_tripit",
+    "tests.fixtures.integration.current_trip",
     "tests.fixtures.integration.aws.api_gateway.create_api_gateway_url",
     "tests.fixtures.integration.aws.api_gateway.get_and_wait_for_lambda_ready",
     "tests.fixtures.integration.aws.api_gateway.access_key",
